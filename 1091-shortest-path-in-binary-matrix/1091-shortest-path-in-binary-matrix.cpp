56403a23fa53ae75b80c91f56a3a4dd7a7172a35
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-01-24 17:05:18 +0530

    Time: 4 ms (98.64%), Space: 23.5 MB (82.56%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,54 +1,51 @@
 class Solution {
 public:
-
-    int r,c ;
-    int row[8] = {-1,-1,-1, 0, 0, 1, 1, 1};
-     int col[8] = {-1, 0, 1,-1, 1,-1, 0, 1};
-     bool valid(int i ,int j)
-     {
-        return i>=0 && i<r && j>=0 && j<c ;
-     }
+   
+   int r,c ;
+   int row[8]= {-1,-1,-1,0,0,1,1,1};
+   int col[8]= {-1,0,1,-1,1,-1,0,1} ;
+   bool valid(int i ,int j)
+   {
+    return i>=0 && i<r && j>=0 && j<c ;
+   }
     
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
-        r=grid.size() ;
-        c=grid[0].size() ;
+         r=grid.size() ;
+         c=grid[0].size() ;
+        int count=0 ;
 
         queue<pair<int,int>>q ;
-        int count=0 ;
-       
-        if(grid[0][0] !=0 || grid[r-1][c-1] !=0)
+        if(grid[0][0] !=0 || grid[r-1][c-1] !=0 )
         return -1 ;
 
-        q.push({0,0}) ;
+        q.push({0,0});
         grid[0][0]=1 ;
         count++ ;
 
-        while(!q.empty()) 
+        while(!q.empty())
         {
             int n=q.size() ;
             while(n--)
             {
-            int new_i=q.front().first ;
-            int new_j=q.front().second ;
-            q.pop() ;
+                int ni=q.front().first ;
+                int nj=q.front().second ;
+                q.pop() ;
 
-            if (new_i == r-1 && new_j == c-1)
-            return count;
+                if(ni==r-1 && nj==c-1)
+                return count ;
 
-            for(int k=0 ;k<8 ;k++)
-            {
-                if(valid(new_i+row[k],new_j+col[k]) && grid[new_i+row[k]][new_j+col[k]]==0)
+                for(int k=0 ;k<8 ;k++)
                 {
-                    grid[new_i+row[k]][new_j+col[k]]=1 ;
-                    q.push({new_i+row[k],new_j+col[k]}) ;
-                    
+                    if(valid(ni+row[k],nj+col[k]) && grid[ni+row[k]][nj+col[k]]==0){
+                        grid[ni+row[k]][nj+col[k]]=1 ;
+                        q.push({ni+row[k],nj+col[k]}) ;
+                    }
                 }
-            }
             }
             count++ ;
         }
-        return -1 ;
 
+
+        return -1 ;
     }
 };
