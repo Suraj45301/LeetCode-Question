7267bdedc87042ce5bf9e0c61d309f6966207d52
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-02-03 15:28:31 +0530

    Time: 302 ms (8.55%), Space: 199.1 MB (10.42%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-
+  
   void DFS(int node ,int parent ,vector<vector<int>>&adj ,vector<int>&dis ,vector<int>&low ,vector<bool>&visited ,vector<vector<int>>&bridges ,int count)
   {
     dis[node]=low[node]=count ;
     visited[node]=1 ;
-
     for(int j=0 ;j<adj[node].size() ;j++)
     {
         int neigh=adj[node][j] ;
@@ -13,12 +12,11 @@ public:
         continue ;
 
         else if(visited[neigh])
-        {
-            low[node]=min(low[node],low[neigh]);
-        }
+        low[node]=min(low[neigh],low[node]);
+
         else{
-            count++ ;
-            DFS(neigh,node,adj,dis,low,visited,bridges,count);
+            count++;
+            DFS(neigh,node ,adj,dis,low,visited,bridges,count);
             if(low[neigh]>dis[node])
             {
                 vector<int>temp ;
@@ -28,23 +26,26 @@ public:
             }
             low[node]=min(low[node],low[neigh]) ;
         }
-    }
-  }
-    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
-        vector<vector<int>>adj(n) ;
-        for(int i=0 ;i<connections.size() ;i++)
-        {
-            int u=connections[i][0] ;
-            int v=connections[i][1];
 
-            adj[u].push_back(v) ;
-            adj[v].push_back(u);
+    }
+
+  }
+  
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+
+        vector<vector<int>>adj(n) ;
+        for(auto edge:connections)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u) ;
         }
         vector<vector<int>>bridges ;
-        vector<int>dis(n);
+        vector<int>dis(n) ;
         vector<int>low(n) ;
         vector<bool>visited(n) ;
-        DFS(0,-1 ,adj,dis,low,visited,bridges,0) ;
+        DFS(0,-1,adj,dis,low,visited,bridges,0) ;
         return bridges ;
     }
 };
