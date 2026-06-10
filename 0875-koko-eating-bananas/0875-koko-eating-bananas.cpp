fbd33c9f6535be5df81b999b880adb26a8482349
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-06-10 16:31:11 +0530

    Time: 7 ms (70.63%), Space: 23 MB (12.35%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
 
-  bool check(vector<int>&piles ,int mid ,int h)
-  {
-    long long ans=0 ;
-
-    for(int i=0 ;i<piles.size() ;i++)
+    bool check(vector<int>&piles ,int mid,int h)
     {
-        ans +=piles[i]/mid ;
-        if(piles[i]%mid !=0)
-        {
-            ans++ ;
-        }
-    }
-    return ans<=h ;
-  }
-    int minEatingSpeed(vector<int>& piles, int h) {
-        
-        int start=1;
-        int end=0 ;
-        int result=0 ;
+        long long ans=0 ;
+
         for(int i=0 ;i<piles.size() ;i++)
         {
-           end=max(end,piles[i]) ;
-        }
-        while(start<=end)
-        {
-            long long mid=start+(end-start)/2 ;
-        
+            ans +=piles[i]/mid ;
 
-            if(check(piles,mid,h))
+            if(piles[i]%mid !=0)
             {
-                result=mid ;
-                end=mid-1 ;
-            }
-            else
-            {
-                start=mid+1 ;
+                ans++ ;
             }
         }
-        return result ;
+        return ans<=h ;
+    }
+
+ 
+    int minEatingSpeed(vector<int>& piles, int h) {
+        
+     int start=1 ;
+     int end=0 ;
+     int result=0 ;
+     for(int i=0 ;i<piles.size() ;i++)
+     {
+        end =max(end,piles[i]) ;
+     }
+     while(start<=end)
+     {
+        int mid=start+(end-start)/2 ;
+
+        if(check(piles,mid,h))
+        {
+            result=mid ;
+            end=mid-1 ;
+        }
+        else{
+            start=mid+1 ;
+        }
+     }
+       return result ;
     }
 };
