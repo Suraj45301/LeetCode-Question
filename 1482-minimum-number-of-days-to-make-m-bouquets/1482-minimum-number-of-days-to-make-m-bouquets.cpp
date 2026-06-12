f5fa5e5618e3ad31b8169f4c05e5ac91846f78d0
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-06-12 20:40:57 +0530

    Time: 14 ms (97.03%), Space: 70.6 MB (11.84%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,56 +1,59 @@
 class Solution {
 public:
+   
+     bool check(vector<int>&bloomDay ,int mid ,int m ,int k)
+     {
+        int flower=0 ;
+        int bounquet=0 ;
 
-   bool check(vector<int>& bloomDay ,int mid ,int m ,int k)
-   {
-    int flower=0 ;
-    int bounquet=0 ;
-
-    for(int i=0 ;i<bloomDay.size() ;i++)
-    {
-        if(bloomDay[i]<=mid)
+        for(int bloom:bloomDay)
         {
-            flower++ ;
-
-            if(flower==k)
+            if(bloom<=mid)
             {
-                bounquet++ ;
+                flower++ ;
+
+                if(flower==k)
+                {
+                    bounquet++ ;
+                    flower=0 ;
+                }
+            }
+            else{
                 flower=0 ;
             }
+
         }
-        else{
-            flower=0 ;
-        }
-    }
-    return bounquet>=m ;
-   }
+        return bounquet>=m ;
+     }
+  
     int minDays(vector<int>& bloomDay, int m, int k) {
         int n=bloomDay.size() ;
-
-        
         if(k>n/m)
         return -1 ;
 
-      int start=0 ,end=0 ;
-      int ans=0 ;
-      for(int i=0 ;i<n ;i++)
-      {
-        end=max(bloomDay[i],end) ;
-        start=min(bloomDay[i],start);
-      }
-      while(start<=end)
-      {
-        int mid=start+(end-start)/2 ;
-        if(check(bloomDay,mid ,m,k))
+        int start=INT_MAX,end=0,ans=0 ;
+        for(int i=0 ;i<bloomDay.size() ;i++)
         {
-            ans=mid ;
-            end=mid-1 ;
+            start=min(start ,bloomDay[i]);
+            end=max(end,bloomDay[i]) ;
         }
-        else{
-            start=mid+1 ;
+
+        while(start<=end)
+        {
+           int mid=start+(end-start)/2 ;
+            
+            if(check(bloomDay ,mid,m,k))
+            {
+                ans=mid ;
+                end=mid-1 ;
+            }
+            else{
+                start=mid+1 ;
+            }
         }
-      }  
-      return ans;     
+
+        
+        return ans ;
 
     }
 };
