deaa5eaae3f3bb8889ab940eabc34fef975f0eec
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-06-13 14:19:04 +0530

    Time: 0 ms (100%), Space: 31.4 MB (45.41%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -10,15 +10,11 @@ public:
         if(nums[mid]==target)
         return mid ;
 
-         if(nums[mid]<target)
-        {
-            start=mid+1 ;
-        }
+        else if(nums[mid]<target)
+        start=mid+1 ;
 
-        else{
-            end=mid-1 ;
-        }
-        
+        else
+        end=mid-1 ;
       }
 
       return -1 ;
