6b266cb47d20693814e2f7cb20fe11c346fb5a6a
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-06-26 22:23:09 +0530

    Time: 0 ms (100%), Space: 26.3 MB (34.18%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -11,11 +11,13 @@ public:
         if(mid%2==1)
         mid-- ;
 
-        if(nums[mid]==nums[ mid+1])
+         if(nums[mid]==nums[mid+1])
         start=mid+2 ;
 
         else
         end=mid ;
+
+
       }
       return nums[start] ;
     }
