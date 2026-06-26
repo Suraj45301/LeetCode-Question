17c5052f15dfdbef8f59e6ada009d2b598dc12be
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-06-26 22:07:02 +0530

    Time: 0 ms (100%), Space: 18.1 MB (41.16%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -8,35 +8,31 @@ public:
         int mid=start+(end-start)/2 ;
 
         if(nums[mid]==target)
-        return 1;
+        return 1 ;
 
         if(nums[start]==nums[mid] && nums[mid]==nums[end])
         {
             start++ ;
             end-- ;
         }
-         
-         else if(nums[start]<=nums[mid])
-         {
-            if(nums[start]<=target && nums[mid]>target)
-            {
-                end=mid-1 ;
-            }
-            else
-            {
-                start=mid+1 ;
-            }
-         }
-         else{
-            if(nums[mid]<target && nums[end]>=target)
-            {
-                start=mid+1 ;
-            }
-            else
-             end=mid-1 ;
-         }
 
+        else if(nums[start]<=nums[mid])
+        {
+            if(nums[start]<=target && nums[mid]>target)
+            end=mid-1 ;
+
+            else
+            start=mid+1 ;
+        }
+        else
+        {
+            if(nums[mid]<target && nums[end]>=target)
+            start=mid+1 ;
+
+            else
+            end=mid-1 ;
+        }
        }
-       return 0 ;
+      return 0 ;
     }
 };
