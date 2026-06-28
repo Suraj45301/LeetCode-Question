4741e571b47bf175bd7605a9363fae44bccc966b
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-06-28 13:31:47 +0530

    Time: 88 ms (81.32%), Space: 192.4 MB (85.36%) - LeetHub

--- 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
+++ 3975-filter-occupied-intervals/3975-filter-occupied-intervals.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
     vector<vector<int>> filterOccupiedIntervals(vector<vector<int>>& occupiedIntervals, int freeStart, int freeEnd) {
-        vector<vector<int>>merge ;
-        sort(occupiedIntervals.begin() ,occupiedIntervals.end()) ;
+       vector<vector<int>>merge ;
+       sort(occupiedIntervals.begin(),occupiedIntervals.end()) ;
 
-        for(auto& it:occupiedIntervals)
+       for(auto &it:occupiedIntervals)
+       {
+        if(merge.empty() || merge.back()[1]+1<it[0])
+        merge.push_back({it});
+
+        else
         {
-           if(merge.empty() || it[0]>merge.back()[1]+1 )
-           {
-            merge.push_back(it) ;
-           }
-           else{
-            merge.back()[1]=max(merge.back()[1] ,it[1]) ;
-           }
+            merge.back()[1]=max(merge.back()[1],it[1]) ;
         }
-        vector<vector<int>>ans ;
-        for(auto& it:merge)
+       }
+
+       vector<vector<int>>ans ;
+       for(auto& it:merge)
+       {
+        int l=it[0],r=it[1] ;
+
+        if(r<freeStart || l>freeEnd)
+        ans.push_back({l,r}) ;
+
+        else
         {
-            int l=it[0],r=it[1] ;
+            if(l<freeStart)
+            ans.push_back({l,freeStart-1}) ;
 
-            if(r<freeStart || l> freeEnd)
-            ans.push_back({l,r});
-
-            else
-            {
-                if(l<freeStart)
-                ans.push_back({l,freeStart-1});
-
-                if(r>freeEnd)
-                ans.push_back({freeEnd+1,r}) ;
-            }
+            if(r>freeEnd)
+            ans.push_back({freeEnd+1,r}) ;
         }
-        return ans ;
+       }
+       return ans ;
     }
 };
