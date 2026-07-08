0746fdaa1fa6be36ec2381f6907c13b20acd30c6
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-07-08 16:31:14 +0530

    Time: 2 ms (78.79%), Space: 7.7 MB (64.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -6,15 +6,16 @@ public:
     
     if(n<=1)
     return n ;
-     vector<int>dp(n+1 ,-1) ;
-     dp[0]=0 ;
-     dp[1]=1 ; 
+     int first=0 ;
+     int second=1 ;
+     int ans=first+second ;
 
-     for(int i=2 ;i<=n ;i++)
+     for(int i=3 ;i<=n ;i++)
      {
-        dp[i]=dp[i-1]+dp[i-2] ;
+        first=second;
+        second=ans ;
+        ans=first+second ;
      }
-
-     return dp[n] ;
+     return ans ;
     }
 };
