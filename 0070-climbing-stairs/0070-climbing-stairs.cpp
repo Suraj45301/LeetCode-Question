d9164c9a09908077d6d3e91781711846c22eb8f0
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-07-08 16:43:06 +0530

    Time: 0 ms (100%), Space: 7.9 MB (72.91%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,14 +8,18 @@ public:
     if(n<=1)
     return 1 ;
 
-    vector<int>dp(n+1) ;
-    dp[0]=1 ;
-    dp[1]=1 ;
+    int first=1 ;
+    int second=1 ;
+    int ans=first+second ;
 
-    for(int i=2 ;i<=n ;i++)
-    dp[i]=dp[i-1]+dp[i-2] ;
+    for(int i=3 ;i<=n ;i++)
+    {
+        first=second ;
+        second=ans ;
+        ans=first+second ;
+    }
 
-    return dp[n] ;
+    return ans ;
 
 
         
