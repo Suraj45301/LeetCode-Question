015daaa7746f8ea963ac74883c01638547349c87
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-07-10 18:05:48 +0530

    Time: 0 ms (100%), Space: 10.8 MB (23.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -19,7 +19,16 @@ public:
     int rob(vector<int>& nums) {
        int n=nums.size() ;
 
+       if(n==1)
+       return nums[0];
        vector<int>dp(n,-1) ;
-      return maxiamount(n-1,nums ,dp) ;
+      dp[0]=nums[0] ;
+      dp[1]=max(nums[1],nums[0]) ;
+
+      for(int i=2 ;i<n ;i++)
+      {
+        dp[i]=max(dp[i-2]+nums[i],dp[i-1]) ;
+      }
+      return dp[n-1] ;
     }
 };
