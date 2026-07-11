96062c9ac1f5c634c7aba42328ad15b2df4367cd
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-07-11 15:19:43 +0530

    Time: 3 ms (6.91%), Space: 10.8 MB (37.14%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -20,13 +20,29 @@ public:
        if(n==1)
        return nums[0] ;
 
+       if (n == 2)
+        return max(nums[0], nums[1]);
+
+
        vector<int> dp1(n, -1);
        vector<int> dp2(n, -1);
 
+       dp1[0]=nums[0] ;
+       dp1[1]=max(nums[0],nums[1]) ;
 
-      int first= maxisum(n-1 ,1,nums,dp1) ; 
-      int second=maxisum(n-2,0,nums,dp2) ;
+       for(int i=2 ;i<n-1 ;i++)
+       {
+        dp1[i]=max(dp1[i-2]+nums[i],dp1[i-1]);
+       }
+      
+        dp2[1]=nums[1] ;
+        dp2[2]=max(nums[1],nums[2]);
 
-      return max(first,second) ;
+        for(int i=3 ;i<n ;i++)
+        {
+            dp2[i]=max(dp2[i-2]+nums[i],dp2[i-1]) ;
+        }
+
+      return max(dp1[n-2],dp2[n-1]) ;
     }
 };
