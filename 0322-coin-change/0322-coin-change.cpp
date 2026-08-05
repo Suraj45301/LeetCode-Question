01ac846ee3faf9fb9ad915fbaca75560bef598d7
Suraj Yadav <surajyaduvanshi27022001@gmail.com>
2026-08-05 18:25:05 +0530

    Time: 17 ms (85.37%), Space: 18 MB (74.25%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
  const int INF = 1e9;
-    int coin(int n ,vector<int>&coins ,int amount,vector<vector<int>>&dp) 
-    {
-
-       if (amount < 0)
-         return INF;
-
-        if (amount == 0)
-         return 0;
-
-       if (n == 0)
-        return INF ;
-
-        if(dp[n][amount] !=-1)
-        return dp[n][amount] ;
-
-     return dp[n][amount]=min(coin(n-1,coins,amount,dp),1+coin(n ,coins,amount-coins[n-1],dp)) ;
-    }
+    
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size() ;
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1)) ;
-       int ans= coin(n ,coins ,amount ,dp ) ;
+       int n=coins.size() ;
 
-        return (ans==INF)?-1:ans ;
+       vector<int>dp(amount+1,1e9) ;
+       dp[0]=0 ;
+
+       for(int coin:coins)
+       {
+         for(int j=coin ;j<=amount ;j++)
+         {
+            dp[j] =min(dp[j],1+dp[j-coin]) ;
+         }
+       }
+       return dp[amount]==1e9 ? -1:dp[amount] ;
     }
 };
